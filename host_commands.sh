3e1b11634b8ca95bda75f0b84b610c020aa73187
sahanracker <sahan.perera@rackspace.co.uk>
2021-03-09 14:25:38 +0000

    Adding initial code

---
+++ host_commands.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#title:         host_commands.sh
+#description:   Execute multiple host commands using AWS SSM
+#author:        Sahan Perera
+#created:       Mar 7 2021
+#updated:       Mar 7 2021
+#version:       V0.1
+#Git    :       https://github.com/rackerlabs/remediation
+#usage:         ./host_commands.sh
+#==============================================================================
+
+source ./variable.txt
+lines=`cat ./$AccountId.txt`
+
+echo "Start the program  at $(date -u) \n"
+echo "============================================================================================"
+echo "Hostname, Instance ID , Host Command output" >> output.csv
+for line in $lines ; do
+    instance_id=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$line" --region $region  --query 'Reservations[].Instances[].InstanceId' --output text)
+    
+    echo "  Gathering install detail of [Hostname:$line | InstanceID:$instance_id ] "
+    echo "========================================================================================="
+    #commandlist=["df -h","echo ****_print_etc_host_****","cat /etc/hosts"]
+    command=$(aws ssm send-command --document-name "AWS-RunShellScript" --comment "Check df -h on $instance_id" --instance-ids $instance_id --parameters commands=["df -h","echo ****_print_etc_host_****","cat /etc/hosts"] --region $region --output text)
+    cmdId=$(echo $command | awk '{print $2}')
+    
+    echo $cmdId
+    while [ "$(aws ssm list-command-invocations --command-id "$cmdId" --query "CommandInvocations[].Status" --output text --region $region )" == "Pending"  ]; do sleep 1; done
+    output=$(aws ssm list-command-invocations --command-id "$cmdId" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --region $region --output json)
+    echo $output
+    echo "$line,$output " >> output.csv
+done
+echo "Finish the program at $(date -u) \n"
